282c03b2165ada9907f9c9712491c6fbc0bd8a63
Entra21 <entra21@proway.treina>
2020-07-06 22:13:28 -0300

    aprendendo os tipos basicos

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace primeira_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome?");
+
+            var myName = Console.ReadLine();
+            
+            
+            Console.WriteLine($"olá,");()
+            
+        }
+    }
+}    
